51caee313d8b6992b7e30254a3ed456612502e62
miguel.campos <miguel.campos@ti.hubfloripa.com.br>
2024-12-29 21:54:17 -0300

    feat: adding nav bar to components nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,14 @@
 		return 'md-icon-button w-14 h-8 mb-1'
 	}
 
+	const checkIfUrlIsEqualAndReturnButton = (url: string) => {
+		if (url === $page.route.id) {
+			return 'md-button-filled whitespace-nowrap h-12 w-56 justify-start';
+		}
+		return 'md-button whitespace-nowrap h-12 text-on-surface w-56 justify-start'
+	}
+
+
 	onMount(() => {
 		theme = document.documentElement.getAttribute('data-theme') || 'light';
 	});
@@ -21,7 +29,7 @@
 	}
 </script>
 
-<nav class="fixed left-0 w-[88px] h-full bg-surface-container py-5 flex flex-col items-center gap-6">
+<nav class="fixed left-0 w-[88px] h-full bg-surface-container py-5 flex flex-col items-center gap-6 z-50">
 	<button disabled class="md-icon-button-filled size-14 cursor-pointer focus:shadow-none shadow-none rounded-2xl" title="Not implemented">search</button>
 	<ul class="flex flex-col items-center gap-6">
 		<li>
@@ -42,9 +50,24 @@
 				<p class="text-on-surface text-xs font-medium text-center">Foundations</p>
 			</a>
 		</li>
+		<li class="flex flex-col items-center justify-center">
+			<button popovertarget="md-drawer" class={checkIfUrlIsEqual('/components') + ' icon-filled'}>add_circle</button>
+			<p class="text-on-surface text-xs font-medium text-center">Components</p>
+		</li>
 	</ul>
 	<button onclick={changeTheme} class="md-icon-button-outlined text-3xl size-12 absolute bottom-4">
 		{ theme === 'dark' ? 'light_mode' : 'dark_mode' }
-		</button>
+	</button>
 </nav>
+<aside id="md-drawer" data-trigger="hover" popover="auto" class="md-drawer">
+	<a href="/components">
+		<button class={checkIfUrlIsEqualAndReturnButton('/components')}>Components overview</button>
+	</a>
+	<details class="md-summary">
+		<summary class="relative"><p>Buttons</p><p class="font-material text-2xl absolute right-3 icon-filled">arrow_drop_down</p></summary>
+		<a href="/components/common-buttons">
+			<button class={checkIfUrlIsEqualAndReturnButton('/components/common-buttons')}>Common Buttons</button>
+		</a>
+	</details>
+</aside>
 {@render children()}
